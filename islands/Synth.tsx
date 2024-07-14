04046bb269fef90be35be980aa6702b9ff9f718e
capo_greco <thomas.capogreco@gmail.com>
2024-07-14 13:45:46 +1000

    root note & is_playing ✅

--- islands/Synth.tsx
+++ islands/Synth.tsx
@@ -4,8 +4,6 @@ import { SynthSplash } from "../components/SynthSplash.tsx"
 import { SynthScreen } from "../components/SynthScreen.tsx"
 import { Program } from "../shared/types.ts"
 
-let ctx: AudioContext
-
 const a = {
    ctx: undefined as AudioContext | undefined,
    osc: undefined as OscillatorNode | undefined,
@@ -15,31 +13,44 @@ const a = {
 const program = {
    versionstamp: `init`,
    is_playing: false,
-   frequency: 440,
-   lag_time: 0.02,
-   amplitude: 0,
+   values: Array (24).fill (0),
+}
+
+const midi_to_freq = (midi: number) => {
+   return 440 * Math.pow (2, (midi - 69) / 12)
 }
 
 const update_graph = () => {
-   if (!a.ctx || !a.osc || !a.amp) return
 
+   let is_populated = true
+   Object.keys (a).forEach (key => {
+      const node_key = key as keyof typeof a
+      if (a[node_key] === undefined) {
+         is_populated = false
+     }
+   })
+   if (!is_populated) return
+
+   if (a.ctx === undefined) return
    const t = a.ctx.currentTime
 
+   if (a.amp === undefined) return
    if (!program.is_playing) {
       a.amp.gain.cancelScheduledValues (t)
       a.amp.gain.setValueAtTime (a.amp.gain.value, t)
-      a.amp.gain.exponentialRampToValueAtTime (0.001, t + program.lag_time)
-      a.amp.gain.linearRampToValueAtTime (0, t + program.lag_time + 0.02)
+      a.amp.gain.exponentialRampToValueAtTime (0.001, t + 0.02)
+      a.amp.gain.linearRampToValueAtTime (0, t + 0.02 + 0.02)
       return
    }
 
+   if (a.osc === undefined) return
    a.osc.frequency.cancelScheduledValues (t)
    a.osc.frequency.setValueAtTime (a.osc.frequency.value, t)
-   a.osc.frequency.exponentialRampToValueAtTime (program.frequency, t + program.lag_time)
+   a.osc.frequency.exponentialRampToValueAtTime (midi_to_freq (program.values[0]), t + 0.02)
 
    a.amp.gain.cancelScheduledValues (t)
    a.amp.gain.setValueAtTime (a.amp.gain.value, t)
-   a.amp.gain.linearRampToValueAtTime (program.amplitude, t + program.lag_time)
+   a.amp.gain.linearRampToValueAtTime (program.values[7] / 127, t + 0.02)
 
 }
 
@@ -60,7 +71,7 @@ export default function Synth (props: {
          if (program.versionstamp === `init` 
             || data.versionstamp > program.versionstamp) {
             Object.assign (program, data)
-            console.log (program)
+            console.log (program.values, program.is_playing)
             update_graph ()
          }
       }
